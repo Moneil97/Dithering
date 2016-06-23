335d2928d1233f8f0e303d8b36818bcd55a07d19
Moneil <cam8059@yahoo.com>
2016-06-23 02:43:46 -0500

    Can convert image to Black and White

--- Dithering/src/Manager.java
+++ Dithering/src/Manager.java
@@ -1,7 +1,33 @@
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
 
 public class Manager {
 
 	public Manager() {
+		
+		//import image pixel array
+		int[][] pixels = null;
+		try {
+			BufferedImage image = ImageIO.read(new File("src/Media/image.jpg"));
+			pixels = Tools.imageToRGBArray(image);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		//Convert to Black and white then save
+		BufferedImage image = Tools.RGBArrayToImage(Dither.toBW(pixels));
+		try {
+			ImageIO.write(image, "jpg", new File("src/Media/imageBW.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
