b2bc8d6a92a23152ecb280fbd42ecce4dd2e7aee
Moneil <cam8059@yahoo.com>
2016-06-24 00:55:32 -0500

    Uses all 3 dithers

--- Dithering/src/Manager.java
+++ Dithering/src/Manager.java
@@ -6,48 +6,59 @@ import javax.imageio.ImageIO;
 public class Manager {
 
 	public Manager() {
-		
-		//int[][] originalpixels = null;
-		
-		//import image pixel array
-		int[][] pixels = null;
+
+		String sourceImageLocation = "src/Media/image.jpg";
+		String extension = "jpg";
+		int[][] originalpixels = null;
+		BufferedImage image = null;
+
+		// import image pixel array
 		try {
-			BufferedImage image = ImageIO.read(new File("src/Media/image.jpg"));
-			//originalpixels = Tools.imageToRGBArray(image);
-			pixels = Tools.imageToRGBArray(image);
+			image = ImageIO.read(new File(sourceImageLocation));
+			originalpixels = Tools.imageToRGBArray(image);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		
-		//Convert to BW w/ Floyd-Steinberg Dithering then save
-		BufferedImage image = Tools.RGBArrayToImage(Dither.toBWFloydSteinbergDither(pixels));
+
+		// Strip off extension
+		sourceImageLocation = sourceImageLocation.substring(0, sourceImageLocation.lastIndexOf("."));
+
+		int[][] pixels = new int[originalpixels.length][originalpixels[0].length];
+
+		// Convert to Black and white then save
+		restore(originalpixels, pixels);
+		image = Tools.RGBArrayToImage(Dither.toBW(pixels));
 		try {
-			ImageIO.write(image, "jpg", new File("src/Media/imageBWFloydSteinbergDither.jpg"));
+			ImageIO.write(image, extension, new File(sourceImageLocation + "BW." + extension));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		//Convert to BW w/ Simple Dithering then save
-//		BufferedImage image = Tools.RGBArrayToImage(Dither.toBWSimpleDither(pixels));
-//		try {
-//			ImageIO.write(image, "jpg", new File("src/Media/imageBWSimpleDither.jpg"));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
-//		//Convert to Black and white then save
-//		BufferedImage image = Tools.RGBArrayToImage(Dither.toBW(pixels));
-//		try {
-//			ImageIO.write(image, "jpg", new File("src/Media/imageBW.jpg"));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
+
+		// Convert to BW w/ Simple Dithering then save
+		restore(originalpixels, pixels);
+		image = Tools.RGBArrayToImage(Dither.toBWSimpleDither(pixels));
+		try {
+			ImageIO.write(image, extension, new File(sourceImageLocation + "BWSimpleDither." + extension));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Convert to BW w/ Floyd-Steinberg Dithering then save
+		restore(originalpixels, pixels);
+		image = Tools.RGBArrayToImage(Dither.toBWFloydSteinbergDither(pixels));
+		try {
+			ImageIO.write(image, extension, new File(sourceImageLocation + "BWFloydSteinbergDither." + extension));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-//	public void restore(int[][] Originalpixels, int[][] pixels){
-//		System.arraycopy(Originalpixels, 0, pixels, 0, pixels.length*pixels[0].length);
-//	}
+
+	public void restore(int[][] originalPixels, int[][] pixels) {
+		for (int i = 0; i < originalPixels.length; i++)
+			System.arraycopy(originalPixels[i], 0, pixels[i], 0, originalPixels[i].length);
+	}
 
 	public static void main(String[] args) {
 		new Manager();
